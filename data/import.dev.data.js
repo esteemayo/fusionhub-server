63898283d0d0f7e5f4e272129e01292e4a399ea0
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-04-10 21:35:45 +0100

    feat: refactor data import script to swap Comment and Reply imports; update package.json for correct script paths

--- data/import.dev.data.js
+++ data/import.dev.data.js
@@ -7,20 +7,20 @@ import path from 'path';
 
 import 'colors';
 
-import Comment from '../models/comment.model.js';
+import Reply from '../models/reply.model.js';
 import Post from '../models/post.model.js';
-import Category from '../models/category.model.js';
+import Comment from '../models/comment.model.js';
 import User from '../models/user.model.js';
-import ReplyComment from '../models/reply.comment.model.js';
+import Category from '../models/category.model.js';
 
 import connectDB from '../config/db.config.js';
 
 dotenv.config({ path: './config.env' });
 
-connectDB();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+connectDB();
 
 const posts = JSON.parse(fs.readFileSync(`${__dirname}/posts.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
@@ -38,11 +38,11 @@ const importData = async () => {
   try {
     console.log('👌✌👌 Loading data...'.cyan.bold);
 
-    await Comment.insertMany(comments);
-    await Post.insertMany(posts);
-    await Category.insertMany(categories);
-    await User.insertMany(users);
-    await ReplyComment.insertMany(replies);
+    await Reply.create(replies);
+    await Post.create(posts);
+    await Comment.create(comments);
+    await User.create(users, { validateBeforeSave: false });
+    await Category.create(categories);
 
     console.log(
       '👍👍👍👍👍👍👍👍 Data successfully loaded! 👍👍👍👍👍👍👍👍'.green.bold,
@@ -58,11 +58,11 @@ const deleteData = async () => {
   try {
     console.log('😢😢 Goodbye Data...'.red.bold);
 
-    await Comment.deleteMany();
+    await Reply.deleteMany();
     await Post.deleteMany();
-    await Category.deleteMany();
+    await Comment.deleteMany();
     await User.deleteMany();
-    await ReplyComment.deleteMany();
+    await Category.deleteMany();
 
     console.log(
       'Data successfully deleted! To load sample data, run\n\n\t npm run sample\n\n'
