850693ceb56641d92c1fe833f5d7b787986c09bd
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-06-16 04:21:03 +0100

    feat: add tokenExpiration to the user data object

--- utils/create.send.google.token.util.js
+++ utils/create.send.google.token.util.js
@@ -1,7 +1,15 @@
 /* eslint-disable */
 
-export const createSendGoogleToken = (user, statusCode, req, res) => {
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+export const createSendGoogleToken = async (user, statusCode, req, res) => {
   const token = user.generateAuthToken();
+
+  const decodedToken = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET,
+  );
 
   res.cookie('authToken', token, {
     expires: new Date(
@@ -16,7 +24,7 @@ export const createSendGoogleToken = (user, statusCode, req, res) => {
 
   const details = {
     ...rest,
-    token,
+    tokenExpiration: decodedToken.exp,
   };
 
   return res.status(statusCode).json({
